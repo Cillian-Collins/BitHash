28e6ac2ed3ca25613aef4fe5e3be109b75b4a8bd
Cillian <Cilliancollins2002@gmail.com>
2017-04-08 14:45:29 +0100

    Version 1.00

--- login.js
+++ login.js
@@ -1,7 +1,6 @@
 function loginUser(){
 var username = document.getElementById("name").value;
 var password = document.getElementById("pass").value;
-
 if ((((((password.length * username.length + username.length) * username.charCodeAt(4))*9999) % 100000) - (username.length - password.length))*999909110199190 == 95044360651763610000){
 	alert("Success!");
 	}
