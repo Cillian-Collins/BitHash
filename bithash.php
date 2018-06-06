5633c4690d5803bb9b74e34027af68fd80e590e1
Cillian <Cilliancollins2002@gmail.com>
2018-06-06 17:46:28 +0100

    Version 2.0
    Uploading Version 2.0 This has been created using PHP. It converts passwords to a MD5 hash, then truncates it and appends a prefix. After which it is then converted to bcrypt. This make it very difficult to crack this hash type.
    
    This includes 2 functions, one for generating the hashes, and a second for verifying that 2 hashes match. This is required for bcrypt as the same input can generate different hashes.

---
+++ bithash.php
@@ -0,0 +1,23 @@
+<?php
+function bithash($pass){
+	//Convert pass to md5.
+	$md5 = md5($pass);
+	//Truncate MD5 in several locations and combine them.
+	$trun_md5 = substr($md5, 1, 5).substr($md5, 3, 14).substr($md5, 12, 13);
+	//Append prefix.
+	$bithash_md5 = '$Bh$'.$trun_md5;
+	//Put it through bcrypt algorithm.
+	$bcrypt = password_hash($bithash_md5, PASSWORD_BCRYPT);
+	//Apply the bithash prefix of 6 bits (to make it a 32 bit hash).
+	return $bcrypt;
+}
+
+function bithash_verify($pass, $hash){
+	$md5 = md5($pass);
+	//Truncate MD5 in several locations and combine them.
+	$trun_md5 = substr($md5, 1, 5).substr($md5, 3, 14).substr($md5, 12, 13);
+	//Append prefix.
+	$bithash_pass = '$Bh$'.$trun_md5;
+	return password_verify($bithash_pass, $hash);
+}
+?>
